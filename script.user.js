733db0149176e5413f95fef9730c8b3991b941b8
DaAwesomeRazor <shzd123@gmail.com>
2019-02-10 13:51:29 -0500

    formatting

--- script.user.js
+++ script.user.js
@@ -9,21 +9,23 @@
 // @grant        unsafeWindow
 // @run-at       document-start
 // ==/UserScript==
-
-const {encode, decode} = msgpack;
+const {
+    encode,
+    decode
+} = msgpack;
 
 class Server {
-	constructor() {
+    constructor() {
         this.queue = new Set();
         this.prog = false;
-		this.ws = null;
+        this.ws = null;
 
         this.connect();
-	}
+    }
 
-	connect() {
-		this.ws = new WebSocket('wss://krunker_social.krunker.io/ws');
-		this.ws.binaryType = 'arraybuffer';
+    connect() {
+        this.ws = new WebSocket('wss://krunker_social.krunker.io/ws');
+        this.ws.binaryType = 'arraybuffer';
         this.ws.onmessage = buf => {
 
             const data = decode(new Uint8Array(buf.data))[1][2];
@@ -34,93 +36,93 @@ class Server {
             this.prog = false;
             this.handleQueue();
         };
-	}
+    }
 
-	closeSocket() {
-		if (this.ws && this.ws.readyState == 1) {
-			this.ws.close();
-			this.ws = null;
-		}
-	}
+    closeSocket() {
+        if (this.ws && this.ws.readyState == 1) {
+            this.ws.close();
+            this.ws = null;
+        }
+    }
 
-	getProfile(user) {
+    getProfile(user) {
         if (this.prog) {
-           this.queue.add(user);
-           return;
+            this.queue.add(user);
+            return;
         }
         console.log('getting ' + user)
-		this.ws.send(encode(['r', ['profile', user, '', null]]));
+        this.ws.send(encode(['r', ['profile', user, '', null]]));
         this.prog = true;
-	}
+    }
 
     handleQueue() {
-        if(this.queue.size >= 1) this.getProfile(Array.from(this.queue)[0]);
+        if (this.queue.size >= 1) this.getProfile(Array.from(this.queue)[0]);
     }
 }
 
 unsafeWindow.KrunkerSocial = class extends Server {
-	getUser(user) {
+    getUser(user) {
         this.getProfile(user);
-	}
+    }
 
-	getLevel(data) {
-		if (!data || typeof data !== 'object' || !data.player_score) return new Error('MUST_SUPPLY', 'data fetched from user');
-		const score = data.player_score;
-		return Math.max(1, Math.floor(0.03 * Math.sqrt(score)));
-	}
+    getLevel(data) {
+        if (!data || typeof data !== 'object' || !data.player_score) return new Error('MUST_SUPPLY', 'data fetched from user');
+        const score = data.player_score;
+        return Math.max(1, Math.floor(0.03 * Math.sqrt(score)));
+    }
 
-	getPlayTime(data) {
-		if (!data || typeof data !== 'object' || !data.player_timeplayed) return new Error('MUST_SUPPLY', 'data fetched from user');
-		const time = data.player_timeplayed;
-		let str = '';
-		const minutes = Math.floor(Math.floor(time / 1000) / 60) % 60;
-		const hours = Math.floor(Math.floor(Math.floor(time / 1000) / 60) / 60) % 24;
-		const days = Math.floor(Math.floor(Math.floor(Math.floor(time / 1000) / 60) / 60) / 24);
-		if (days) str += `${days}d `;
-		if (hours) str += `${hours}h `;
-		if (minutes) str += `${minutes}m`;
-		return str;
-	}
+    getPlayTime(data) {
+        if (!data || typeof data !== 'object' || !data.player_timeplayed) return new Error('MUST_SUPPLY', 'data fetched from user');
+        const time = data.player_timeplayed;
+        let str = '';
+        const minutes = Math.floor(Math.floor(time / 1000) / 60) % 60;
+        const hours = Math.floor(Math.floor(Math.floor(time / 1000) / 60) / 60) % 24;
+        const days = Math.floor(Math.floor(Math.floor(Math.floor(time / 1000) / 60) / 60) / 24);
+        if (days) str += `${days}d `;
+        if (hours) str += `${hours}h `;
+        if (minutes) str += `${minutes}m`;
+        return str;
+    }
 
-	getKDR(data) {
-		if (!data || typeof data !== 'object' || !data.player_kills || !data.player_deaths) return new Error('MUST_SUPPLY', 'data fetched from user');
-		const KDR = data.player_kills / data.player_deaths || 0;
-		return KDR.toFixed(2);
-	}
+    getKDR(data) {
+        if (!data || typeof data !== 'object' || !data.player_kills || !data.player_deaths) return new Error('MUST_SUPPLY', 'data fetched from user');
+        const KDR = data.player_kills / data.player_deaths || 0;
+        return KDR.toFixed(2);
+    }
 
-	getWL(data) {
-		if (!data || typeof data !== 'object' || !data.player_wins || !data.player_games_played) return new Error('MUST_SUPPLY', 'data fetched from user');
-		const WL = data.player_wins / data.player_games_played || 0;
-		return WL.toFixed(2);
-	}
+    getWL(data) {
+        if (!data || typeof data !== 'object' || !data.player_wins || !data.player_games_played) return new Error('MUST_SUPPLY', 'data fetched from user');
+        const WL = data.player_wins / data.player_games_played || 0;
+        return WL.toFixed(2);
+    }
 
-	getSPK(data) {
-		if (!data || typeof data !== 'object' || !data.player_score || !data.player_kills) return new Error('MUST_SUPPLY', 'data fetched from user');
-		const SPK = data.player_score / data.player_kills || 0;
-		return SPK.toFixed(2);
-	}
+    getSPK(data) {
+        if (!data || typeof data !== 'object' || !data.player_score || !data.player_kills) return new Error('MUST_SUPPLY', 'data fetched from user');
+        const SPK = data.player_score / data.player_kills || 0;
+        return SPK.toFixed(2);
+    }
 
-	getSimplified(data) {
-		return {
-			name: data.player_name,
-			id: data.player_id,
-			score: data.player_score,
-			level: this.getLevel(data),
-			kills: data.player_kills,
-			deaths: data.player_deaths,
-			kdr: this.getKDR(data),
-			spk: this.getSPK(data),
-			totalGamesPlayed: data.player_games_played,
-			wins: data.player_wins,
-			loses: data.player_games_played - data.player_wins,
-			wl: this.getWL(data),
-			playTime: this.getPlayTime(data),
-			krunkies: data.player_funds,
-			clan: data.player_clan ? data.player_clan : 'No Clan',
-			featured: data.player_featured ? data.player_featured : 'No',
-			hacker: data.player_hack ? data.player_hack : 'No'
-		};
-	}
+    getSimplified(data) {
+        return {
+            name: data.player_name,
+            id: data.player_id,
+            score: data.player_score,
+            level: this.getLevel(data),
+            kills: data.player_kills,
+            deaths: data.player_deaths,
+            kdr: this.getKDR(data),
+            spk: this.getSPK(data),
+            totalGamesPlayed: data.player_games_played,
+            wins: data.player_wins,
+            loses: data.player_games_played - data.player_wins,
+            wl: this.getWL(data),
+            playTime: this.getPlayTime(data),
+            krunkies: data.player_funds,
+            clan: data.player_clan ? data.player_clan : 'No Clan',
+            featured: data.player_featured ? data.player_featured : 'No',
+            hacker: data.player_hack ? data.player_hack : 'No'
+        };
+    }
 }
 
 
@@ -132,42 +134,42 @@ Error.prototype = {};
 
 
 unsafeWindow.HackDetect = class {
-	static init() {
-		this.api = new KrunkerSocial();
-		this.players = new Set();
+    static init() {
+        this.api = new KrunkerSocial();
+        this.players = new Set();
 
 
-		setInterval(this.getUsers.bind(this), 1000);
-	}
+        setInterval(this.getUsers.bind(this), 1000);
+    }
 
-	static getUsers() {
-		const players = new Set();
-		Array.from($("#leaderContainer").children()).forEach(child => {
-           if ($(child).find(".leaderName")[0]) {
-               let name = $(child).find(".leaderName")[0].innerHTML;
-               if (name.indexOf("<span") >= 0) name = name.split("<span")[0];
-               if (name.indexOf("Guest") < 0 && name !== "check_circle") players.add(name);
-           }
+    static getUsers() {
+        const players = new Set();
+        Array.from($("#leaderContainer").children()).forEach(child => {
+            if ($(child).find(".leaderName")[0]) {
+                let name = $(child).find(".leaderName")[0].innerHTML;
+                if (name.indexOf("<span") >= 0) name = name.split("<span")[0];
+                if (name.indexOf("Guest") < 0 && name !== "check_circle") players.add(name);
+            }
 
-		})
-		let difference = new Set([...players].filter(x => !this.players.has(x)));
+        })
+        let difference = new Set([...players].filter(x => !this.players.has(x)));
 
-		difference.forEach(player => this.checkPlayer(player));
+        difference.forEach(player => this.checkPlayer(player));
 
-		this.players = players;
-	}
+        this.players = players;
+    }
 
-	static checkPlayer(player) {
+    static checkPlayer(player) {
         console.log(player)
-		const user = this.api.getUser(player);
+        const user = this.api.getUser(player);
 
-	}
+    }
 
     static handlePlayer(stats) {
         console.log(stats)
         if (stats.player_hack) {
 
-           $("#chatList").append(`<div class="chatItem"><span class="chatMsg"><span style="color:#64b5f6">${stats.player_name}</span> is a flagged <span style="color:#eb5656">HACKER</span></span></div><br>`)
+            $("#chatList").append(`<div class="chatItem"><span class="chatMsg"><span style="color:#64b5f6">${stats.player_name}</span> is a flagged <span style="color:#eb5656">HACKER</span></span></div><br>`)
         }
     }
 
